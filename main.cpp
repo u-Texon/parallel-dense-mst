137f544b5696582712c6eb26e400eec1f19754d3
David Bumm <uusiy@student.kit.edu>
2023-04-18 21:00:05 +0200

    timer example finally compiles

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 
 int main() {
         std::cout << "Programm started" << std::endl;
-    hybridMST::get_timer().start("timer", 0);
+    hybridMST::Timer timer;
+    timer.start("timer", 0);
         std::cout << "Timer started" << std::endl;
 
     //int solution = 23490234 * 247238423894789;
-    hybridMST::get_timer().stop("timer", 0);
+   timer.start("timer", 0);
         std::cout << "Timer stopped" << std::endl;
     return 0;
 }
