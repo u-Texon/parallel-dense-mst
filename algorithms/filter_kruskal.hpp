87ed2d138b6bdbd20068804a51db8ebee8510626
David Bumm <uusiy@student.kit.edu>
2023-04-22 22:03:14 +0200

    compared run time for filter-kruskal

--- algorithms/filter_kruskal.hpp
+++ algorithms/filter_kruskal.hpp
@@ -4,17 +4,17 @@
 
 namespace filterKruskal {
 
-    inline WEdge pickPivot(WEdgeList &edges) {
-        return edges[edges.size() / 2];
+    inline WEdge pickPivot(WEdgeList *edges) {
+        return edges->at(edges->size()/2);
     }
 
     inline int threshold() {
-        return 10; //TODO: determine threshold;
+        return 30; //TODO: determine threshold;
     }
 
-    inline WEdgeList filter(WEdgeList &edges, UnionFind *uf) {
+    inline WEdgeList filter(WEdgeList *edges, UnionFind *uf) {
         WEdgeList newList;
-        for (auto edge: edges) {
+        for (auto edge: *edges) {
             int f1 = uf->find(edge.get_src());
             int f2 = uf->find(edge.get_dst());
             if (f1 != f2) {
@@ -25,26 +25,25 @@ namespace filterKruskal {
     }
 
 
-    inline WEdgeList getMST(WEdgeList &edges, UnionFind *uf) {
-        if (edges.size() <= threshold()) {
+    inline WEdgeList getMST(WEdgeList *edges, UnionFind *uf) {
+        if (edges->size() <= threshold()) {
             return kruskal::getMST(edges, uf);
         }
 
         WEdge pivot = pickPivot(edges);
         WEdgeList smaller;
         WEdgeList bigger;
-        for (auto edge: edges) {
+        for (auto edge: *edges) {
             if (edge.get_weight() <= pivot.get_weight()) {
                 smaller.push_back(edge);
             } else {
                 bigger.push_back(edge);
             }
         }
-        assert(smaller.size() + bigger.size() == edges.size());
         WEdgeList mst;
-        mst = filterKruskal::getMST(smaller, uf);
-        bigger = filter(bigger, uf);
-        WEdgeList bigEdges = filterKruskal::getMST(bigger, uf);
+        mst = filterKruskal::getMST(&smaller, uf);
+        bigger = filter(&bigger, uf);
+        WEdgeList bigEdges = filterKruskal::getMST(&bigger, uf);
         mst.insert(mst.end(), bigEdges.begin(), bigEdges.end());
 
         return mst;
--- main.cpp
+++ main.cpp
@@ -6,55 +6,59 @@
 #include "algorithms/filter_kruskal.hpp"
 
 int main() {
-        std::cout << "Program started" << std::endl;
     hybridMST::Timer timer;
-    timer.start("timer", 0);
-        std::cout << "Timer started" << std::endl;
-
-    //int solution = 23490234 * 247238423894789;
-    timer.stop("timer", 0);
-
-        std::cout << "Timer stopped with output: " << timer.output()  << std::endl;
 
 
-    int edgeCount = 100;
-    int vertexCount = 20;
+    int edgeCount = 1000;
+    int vertexCount = 50;
     int maxWeight = 40;
 
 
     std::vector<WEdge> edges = generate::randomGraph(edgeCount, vertexCount, maxWeight);
 
-    std::cout << "edges are :" << std::endl;
-    for (auto & edge : edges)
-    {
+    // std::cout << "edges are :" << std::endl;
+    for (auto &edge: edges) {
 
-        std::cout << "(" << edge.get_src() << "," << edge.get_dst() << "," << edge.get_weight() << ") ";
+        // std::cout << "(" << edge.get_src() << "," << edge.get_dst() << "," << edge.get_weight() << ") ";
     }
 
     std::cout << std::endl;
 
     UnionFind uf(vertexCount);
-    std::vector<WEdge> mst = kruskal::getMST(edges, &uf);
-    std::cout << "MST-edges are :" << std::endl;
+
+
+    timer.start("timer", 0);
+    std::vector<WEdge> mst = kruskal::getMST(&edges, &uf);
+    timer.stop("timer", 0);
+    std::cout << "Kruskal stopped with output: " << timer.output() << std::endl;
+    timer.reset();
+
+    // std::cout << "MST-edges are :" << std::endl;
     int kruskalWeight = 0;
-    for (auto & edge: mst) {
-        std::cout << "(" << edge.get_src() << "," << edge.get_dst() << "," << edge.get_weight() << ") ";
+    for (auto &edge: mst) {
+        //std::cout << "(" << edge.get_src() << "," << edge.get_dst() << "," << edge.get_weight() << ") ";
         kruskalWeight += edge.get_weight();
     }
     std::cout << std::endl;
 
 
     UnionFind uf2(vertexCount);
-    std::vector<WEdge> mst2 = filterKruskal::getMST(edges, &uf2);
+
+    timer.start("timer", 0);
+    std::vector<WEdge> mst2 = filterKruskal::getMST(&edges, &uf2);
+    timer.stop("timer", 0);
+    std::cout << "Filter Kruskal stopped with output: " << timer.output() << std::endl;
+
     int filterWeight = 0;
-    for (auto & edge: mst2) {
-        std::cout << "(" << edge.get_src() << "," << edge.get_dst() << "," << edge.get_weight() << ") ";
+    for (auto &edge: mst2) {
+        //std::cout << "(" << edge.get_src() << "," << edge.get_dst() << "," << edge.get_weight() << ") ";
         filterWeight += edge.get_weight();
     }
     std::cout << std::endl;
 
     std::cout << "kruskal found MST with weight: " << kruskalWeight << std::endl;
     std::cout << "filter-kruskal found MST with weight: " << filterWeight << std::endl;
+
 
     return 0;
 }
