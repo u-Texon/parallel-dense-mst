177a89f40f7b36b73f5ee416cf7be34bddddb9e6
David Bumm <uusiy@student.kit.edu>
2023-06-26 16:15:31 +0200

    changes to filter kruskal

--- algorithms/filter_kruskal.hpp
+++ algorithms/filter_kruskal.hpp
@@ -3,51 +3,52 @@
 #include "kruskal.hpp"
 
 #define maxLoops 10
+#define pivotEdgeCount 10
 
 namespace filterKruskal {
 
     template<typename Edge>
-    inline Edge pickPivot(std::vector<Edge> &edges) { //TODO:alternative: pick 3 elements
-        int i = rand() % ( edges.size());
-        return edges[i];
+    inline VId pickPivot(std::vector<Edge> &edges) {
+        VId pivotWeight = 0;
+        for (int i = 0; i < pivotEdgeCount; ++i) {
+            VId w = rand() % (edges.size());
+            w = edges[w].get_weight();
+            pivotWeight += w;
+        }
+        return (VId) (pivotWeight/pivotEdgeCount);
     }
 
     inline int threshold(const VId &n) {
-        int t = 3 * n; //TODO: is this a good threshold?
+        int t = 3 * n;
         return t;
     }
 
     template<typename Edge>
-    inline std::vector<Edge> filter(std::vector<Edge> &edges, UnionFind &uf) {
-        //TODO: remove if
-        std::vector<Edge> newList;
-        for (auto &edge: edges) {
+    inline void filter(std::vector<Edge> &edges, UnionFind &uf) {
+        std::remove_if(edges.begin(), edges.end(), [&](const auto &edge) {
             VId f1 = uf.find(edge.get_src());
             VId f2 = uf.find(edge.get_dst());
-            if (f1 != f2) {
-                newList.push_back(edge);
-            }
-        }
-        return newList;
+            return f1 == f2;
+        });
     }
 
 
     template<typename Edge>
     inline std::vector<Edge> getMST(VId n, std::vector<Edge> &edges, UnionFind &uf, VId &counter) {
-        int t = threshold(n);
-        if (edges.size() <= t) {
+        if (edges.size() <= threshold(n)) {
             return kruskal::getMST(edges, uf);
         }
-        Edge pivot = pickPivot(edges);
-        std::vector<Edge> smaller;
-        std::vector<Edge> bigger;
-        for (auto &edge: edges) { //TODO: std::partition std::span
-            if (edge.get_weight() <= pivot.get_weight()) {
-                smaller.push_back(edge);
-            } else {
-                bigger.push_back(edge);
-            }
-        }
+        VId pivotWeight = pickPivot(edges);
+
+
+        auto middle = std::partition(edges.begin(), edges.end(), [&](const auto &edge)
+        {
+            return edge.get_weight() <= pivotWeight;
+        });
+        std::vector<Edge> smaller(edges.begin(), middle);
+        std::vector<Edge> bigger(middle, edges.end());
+
+
 
         if (bigger.empty()) { //error prevention for infinite loop
             counter++;
@@ -62,7 +63,7 @@ namespace filterKruskal {
         //TODO: vector der größe n und dann immer einfügen wenn kante gefunden
         std::vector<Edge> mst;
         mst = filterKruskal::getMST(n, smaller, uf, counter);
-        bigger = filter(bigger, uf);
+        filter(bigger, uf);
         std::vector<Edge> bigEdges = filterKruskal::getMST(n, bigger, uf, counter);
         mst.insert(mst.end(), bigEdges.begin(), bigEdges.end());
 
